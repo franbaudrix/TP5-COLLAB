d1cf49789ba8d148b49f949f5a29e278db6fe2d8
Fran Baudrix <franco39239@gmail.com>
2024-06-11 15:58:00 -0300

    clima 2.0

--- TP5/parte2/baudrix_velez-P6.sh
+++ TP5/parte2/baudrix_velez-P6.sh
@@ -2,6 +2,8 @@ echo -n "Ingrese la ciudad que desea consultar el clima: "
 read CIUDAD
 
 API_KEY="bcda1f43e3d340b7b1701001241106"
+
+#convierte los espacios de la ciudad ingresada en %20 para ser utilizados en la url
 CIUDAD=$(echo "$CIUDAD" | sed 's/ /%20/g')
 
 URL="https://api.weatherapi.com/v1/current.json?key=$API_KEY&q=$CIUDAD&aqi=yes"
@@ -15,12 +17,11 @@ response=$(curl -s "$URL")
 FECHA=$(echo "$response" | jq '.current.last_updated')
 
 #chequea si hay un error antes de obtener los datos del clima
-if [ "$?" -ne 0 ]; then
+if [ "$?" -ne 0 ] || [ "$FECHA" = "null" ]; then
 	echo "Error"
 	exit 1
 fi
 
-if [ "$FECHA" ]; then
 #obtiene los datos del clima necesarios desde el response
 TEMPERATURA=$(echo "$response" | jq '.current.temp_c')
 TEXT=$(echo "$response" | jq '.current.condition.text')
@@ -39,13 +40,31 @@ VIENTO_DIR=$(echo "$VIENTO_DIR" | sed 's/"//g')
 
 
 clear
-echo " ########## CLIMA $CIUDAD ##########"
+#graficos para mostrar los datos del clima
+echo " ############ CLIMA $CIUDAD ############"
 echo ""
-echo " $TEXT"
+echo " Temperatura: $TEMPERATURA°C"
 echo ""
-echo " Temperatura: $TEMPERATURA°C           $VIENTO_DIR"
-echo " Humedad del $HUMEDAD%               $VIENTO_VEL km/h"
+
+echo -n " $TEXT"
+#for loop usado para definir la distancia entre texto y direccion del viento
+tamanio=${#TEXT}
+let tamanio=27-tamanio
+for i in $(seq 1 $tamanio)
+do
+echo -n " "
+done
+echo "$VIENTO_DIR"
+
+echo -n " Humedad del $HUMEDAD%"
+#for loop usado para definir la distancia entre humedad  y velocidad del viento
+tamanio=${#HUMEDAD}
+let tamanio=28-tamanio-14
+for i in $(seq 1 $tamanio)
+do
+echo -n " "
+done
+
+echo "$VIENTO_VEL km/h"
 echo ""
 echo " Actualizado al $FECHA"
-
-fi
