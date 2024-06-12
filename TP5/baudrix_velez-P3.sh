cd12e0215f8243daf211af5e1780aa8dff0e73d0
Fran Baudrix <franco39239@gmail.com>
2024-06-12 12:34:48 -0300

    calculadora integrada y manual terminado

--- TP5/baudrix_velez-P3.sh
+++ TP5/baudrix_velez-P3.sh
@@ -15,22 +15,31 @@ div_cero() {
 }
 
 hacer_cuenta_y_error_handling() {
-	local numero1="$numero1"
-	local numero2="$numero2"
 	local operacion="$operacion"
-	if [[ $operacion = "/" && $numero2 = 0 ]]; then
+	local titulo="$titulo"
+	clear
+        graphics "$titulo"
+        read num1
+
+        clear
+        cad=$num1$operacion
+        graphics "$titulo"
+        read -p "$cad" num2
+
+	if [[ $operacion = "/" && $num2 = 0 ]]; then
 		div_cero
-	elif [ "$(echo "$numero2" | bc)" != "$numero2" ]; then
+	elif [ "$(echo "$num2" | bc)" != "$num2" ]; then
                 zenity --error --no-wrap \
 			--text="Ingresaste un valor incorrecto,\nsolo podes ingresar numeros!"
-        elif [ "$(echo "$numero1" | bc)" != "$numero1" ]; then
+        elif [ "$(echo "$num1" | bc)" != "$num1" ]; then
 		zenity --error --no-wrap \
 			--text="Ingresaste un valor incorrecto,\nsolo podes ingresar numeros!"
         else
-		total=$(echo "scale=5; $numero1 $operacion $numero2" | bc)
-                echo ""
-                echo "--> $numero1 $operacion $numero2 = $total"
-                echo ""
+		clear
+        	graphics "$titulo"
+        	total=$(echo "scale=5; $num1 $operacion $num2" | bc)
+        	echo "$num1 $operacion $num2 = $total"
+
         	while true; do
                 	echo -n "Ingresa x para volver al menu: "
                 	read -r -n 2 volver
@@ -43,70 +52,60 @@ hacer_cuenta_y_error_handling() {
 	fi
 }
 
+
+
+graphics() {
+	echo "################################"
+	tamanio=${#1}
+	let tamanio=tamanio/2
+	let tamanio=14-tamanio
+	tit=""
+	for i in $(seq 1 $tamanio)
+	do
+	tit="$tit "
+	done
+	echo "#$tit $1 $tit#"
+	echo "################################"
+}
+
 while true; do
 clear
-echo "###############################"
-echo "#         CALCULADORA         #"
-echo "###############################"
-echo "#                             #"
-echo "# Opcion[1] - Suma            #"
-echo "# Opcion[2] - Resta           #"
-echo "# Opcion[3] - Multiplicacion  #"
-echo "# Opcion[4] - Division        #"
-echo "# Opcion[5] - Manual De Uso   #"
-echo "# Opcion[0] - Salir           #"
-echo "#                             #"
-echo "###############################"
+graphics "MENU"
+echo "#                              #"
+echo "# Opcion[1] - Suma             #"
+echo "# Opcion[2] - Resta            #"
+echo "# Opcion[3] - Multiplicacion   #"
+echo "# Opcion[4] - Division         #"
+echo "# Opcion[5] - Manual De Uso    #"
+echo "# Opcion[0] - Salir            #"
+echo "#                              #"
+echo "################################"
 echo -n "Ingresa tu eleccion: "
 read opcion
 
 if [ "$opcion" -eq 1 ]; then
 	clear
 	operacion="+"
-	echo "###############################"
-	echo "#            SUMA             #"
-	echo "###############################"
-	echo -n "Ingresa un numero para sumar: "
-	read numero1
-	echo -n "Ingresa otro numero para sumar: "
-	read numero2
-	hacer_cuenta_y_error_handling numero1 numero2 operacion
+	titulo="SUMA"
+	hacer_cuenta_y_error_handling operacion titulo
 elif [ "$opcion" -eq 2 ]; then 
 	clear
 	operacion="-"
-	echo "###############################"
-        echo "#            RESTA            #"
-        echo "###############################"
-        echo -n "Ingresa un numero para ser restado: "
-        read numero1
-        echo -n "Ingresa el numero que le quieres restar: "
-        read numero2
-	hacer_cuenta_y_error_handling numero1 numero2 operacion
+        titulo="RESTA"
+        hacer_cuenta_y_error_handling operacion titulo
 elif [ "$opcion" -eq 3 ]; then
 	clear
 	operacion="*"
-	echo "###############################"
-        echo "#       MULTIPLICACION        #"
-        echo "###############################"
-        echo -n "Ingresa un numero para ser multiplicado: "
-        read numero1
-        echo -n "Ingresa el numero por el que lo quieres multiplicar: "
-        read numero2
-	hacer_cuenta_y_error_handling numero1 numero2 operacion
+	titulo="MULTIPLICACION"
+        hacer_cuenta_y_error_handling operacion titulo
 elif [ "$opcion" -eq 4 ]; then
 	clear
 	operacion="/"
-	echo "###############################"
-        echo "#          DIVISION           #"
-        echo "###############################"
-        echo -n "Ingresa un numerador de la division: "
-        read numero1
-        echo -n "Ingresa el denominador de la division: "
-        read numero2
-	hacer_cuenta_y_error_handling numero1 numero2 operacion
+	titulo="DIVISION"
+        hacer_cuenta_y_error_handling operacion titulo
 elif [ "$opcion" -eq 5 ]; then
 	zenity --info --no-wrap \
-               --text="MANUAL DE USO\n\nLa calculadora funciona de tal manera y cuando estas ahi pasa tal xd lol\nydespues hace tal y tal pero cuando vas a ahcer suma pasa otra cosa\nentonces en ese caso vamos a ser repiolas" 
+               --text="MANUAL DE USO\n\n Luego de seleccionar la operacion deseada, siga estos pasos:\n\n 1. Ingrese el primer numero y presione enter. \n\n 2. Ingrese el segundo numero y presione enter nuevamente. \n\n 3. El resultado de su cuenta se mostrara por pantalla. \n\n 4. Para volver al menu principal, presiente la letra X." 
 elif [ "$opcion" -eq 0 ]; then
 	clear
 	break;
