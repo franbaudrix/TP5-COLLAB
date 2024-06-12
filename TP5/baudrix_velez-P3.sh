cb648f5893f0c866073177207da0b587fb50807b
00karim <karimveleza@gmail.com>
2024-06-12 02:15:32 -0300

    calculadora casi terminada

--- TP5/baudrix_velez-P3.sh
+++ TP5/baudrix_velez-P3.sh
@@ -1,30 +1,46 @@
-hacer_cuenta() {
+div_cero() {
+	echo ""
+	echo "Error! No es posible dividir por 0"
+	echo ""
+	while true; do
+      		read -p "Ingresa x para volver al menu: " volver
+      		if [[ $volver == $'x' ]]; then
+         		break
+      		else
+			echo ""
+         		echo "Error! Opcion inexistente"
+      			echo ""
+		fi
+	done
+}
+
+hacer_cuenta_y_error_handling() {
 	local numero1="$numero1"
 	local numero2="$numero2"
 	local operacion="$operacion"
-	total=$(echo "scale=5; $numero1 $operacion $numero2" | bc)
-	if [ "$(echo "$numero2" | bc)" != "$numero2" ]; then
-                echo ""
-                echo "Ingresaste un valor incorrecto, solo podes ingresar numeros!"
-                echo ""
+	if [[ $operacion = "/" && $numero2 = 0 ]]; then
+		div_cero
+	elif [ "$(echo "$numero2" | bc)" != "$numero2" ]; then
+                zenity --error --no-wrap \
+			--text="Ingresaste un valor incorrecto,\nsolo podes ingresar numeros!"
         elif [ "$(echo "$numero1" | bc)" != "$numero1" ]; then
-                echo ""
-                echo "Ingresaste un valor incorrecto, solo podes ingresar numeros!"
-                echo ""
+		zenity --error --no-wrap \
+			--text="Ingresaste un valor incorrecto,\nsolo podes ingresar numeros!"
         else
+		total=$(echo "scale=5; $numero1 $operacion $numero2" | bc)
                 echo ""
                 echo "--> $numero1 $operacion $numero2 = $total"
                 echo ""
-        fi
-        while true; do
-                echo -n "Ingresa x para volver al menu: "
-                read -r -n 2 volver
-                if [[ $volver == $'x' ]]; then
-                        break
-                else
-                        echo "Opcion invalida, para volver al menu ingresa x"
-                fi 
-        done
+        	while true; do
+                	echo -n "Ingresa x para volver al menu: "
+                	read -r -n 2 volver
+                	if [[ $volver == $'x' ]]; then
+                        	break
+                	else
+                        	echo "Opcion invalida, para volver al menu ingresa x"
+                	fi 
+        	done
+	fi
 }
 
 while true; do
@@ -37,6 +53,7 @@ echo "# Opcion[1] - Suma            #"
 echo "# Opcion[2] - Resta           #"
 echo "# Opcion[3] - Multiplicacion  #"
 echo "# Opcion[4] - Division        #"
+echo "# Opcion[5] - Manual De Uso   #"
 echo "# Opcion[0] - Salir           #"
 echo "#                             #"
 echo "###############################"
@@ -53,7 +70,7 @@ if [ "$opcion" -eq 1 ]; then
 	read numero1
 	echo -n "Ingresa otro numero para sumar: "
 	read numero2
-	hacer_cuenta numero1 numero2 operacion
+	hacer_cuenta_y_error_handling numero1 numero2 operacion
 elif [ "$opcion" -eq 2 ]; then 
 	clear
 	operacion="-"
@@ -64,7 +81,7 @@ elif [ "$opcion" -eq 2 ]; then
         read numero1
         echo -n "Ingresa el numero que le quieres restar: "
         read numero2
-	hacer_cuenta numero1 numero2 operacion
+	hacer_cuenta_y_error_handling numero1 numero2 operacion
 elif [ "$opcion" -eq 3 ]; then
 	clear
 	operacion="*"
@@ -75,8 +92,8 @@ elif [ "$opcion" -eq 3 ]; then
         read numero1
         echo -n "Ingresa el numero por el que lo quieres multiplicar: "
         read numero2
-	hacer_cuenta numero1 numero2 operacion 
-elif [ "$opcion" -eq 4 ]; then 
+	hacer_cuenta_y_error_handling numero1 numero2 operacion
+elif [ "$opcion" -eq 4 ]; then
 	clear
 	operacion="/"
 	echo "###############################"
@@ -86,34 +103,10 @@ elif [ "$opcion" -eq 4 ]; then
         read numero1
         echo -n "Ingresa el denominador de la division: "
         read numero2
-	echo ""
-        if [ "$(echo "$numero2divi" | bc)" != "$numero2divi" ]; then
-                echo ""
-                echo "Ingresaste un valor incorrecto, solo podes ingresar numeros!"
-                echo ""
-		while true; do
-                        echo -n "Ingresa x para volver al menu: "
-                        read -r -n 2 volver
-                        if [[ $volver == $'x' ]]; then
-                                break
-                        else
-                                echo "Opcion invalida, para volver al menu ingresa x"
-                        fi 
-                done
-	elif [ "$(echo "$numero2divi" | bc)" -eq 0 ]; then
-		echo -n "Error! No es posible dividir por 0"
-		while true; do
-                	echo -n "Ingresa x para volver al menu: "
-                	read -r -n 2 volver
-                	if [[ $volver == $'x' ]]; then
-                        	break
-                	else
-                        	echo "Opcion invalida, para volver al menu ingresa x"
-                	fi 
-       		done
-	else
-		hacer_cuenta numero1divi numero2divi operacion
-	fi
+	hacer_cuenta_y_error_handling numero1 numero2 operacion
+elif [ "$opcion" -eq 5 ]; then
+	zenity --info --no-wrap \
+               --text="MANUAL DE USO\n\nLa calculadora funciona de tal manera y cuando estas ahi pasa tal xd lol\nydespues hace tal y tal pero cuando vas a ahcer suma pasa otra cosa\nentonces en ese caso vamos a ser repiolas" 
 elif [ "$opcion" -eq 0 ]; then
 	clear
 	break;
